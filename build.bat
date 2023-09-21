2cf9876e94f50e1935f340de0899ce75d753ebad
dmitry.solonina <dmitry.solonina@b-next.com>
2023-09-21 11:52:54 +0200

    add JSF project

--- build.bat
+++ build.bat
@@ -1,20 +1,8 @@
 @echo off
-
-rem create a file named b-next.bat and place it in your ~\\b-next folder with the following content (changed to your location of java and maven).
 set JAVA_HOME=c:\java\jdk-17.0.6
 set MAVEN_HOME=c:\java\apache-maven
-
-rem call %userprofile%\.bnext\b-next.bat
-
 set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
+
 java -version
 
-if %1.==. (
-    echo No parameters have been provided - using default profile
-    set PORTAL_PROFILE=helaba-qa
-) else (
-    echo Using profile: %1
-    set PORTAL_PROFILE=%1
-)
-
-mvn -Dmaven.test.skip=false clean install -P%PORTAL_PROFILE%
+mvn -Dmaven.test.skip=false clean install
