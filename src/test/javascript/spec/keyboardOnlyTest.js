e874ebd1b51e8750ee41f458507dce9e13abe40c
dsolonina <dsolonina@gmail.com>
2024-07-03 14:13:24 +0300

    add JS test

--- src/test/javascript/spec/keyboardOnlyTest.js
+++ src/test/javascript/spec/keyboardOnlyTest.js
@@ -1,5 +1,5 @@
 describe("addEmp Test", function() {
     it("should return addemp", function() {
         const result = "111";
-        expect(result).toContain("addEmp")
+        expect(result).toContain("111")
 })});
